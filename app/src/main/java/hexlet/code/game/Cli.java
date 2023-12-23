bbac7139bcd60c1e6a7965e9ec91e7b575f66114
Сергей <polwen@yandex.ru>
2023-12-23 11:07:10 +0300

    fix gradle

--- app/src/main/java/hexlet/code/game/Cli.java
+++ app/src/main/java/hexlet/code/game/Cli.java
@@ -17,7 +17,7 @@ public class Cli {
         System.out.println("Welcome to the Brain IsEvenGame!");
         System.out.print("May I have your name? ");
         String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + name);
         return name;
     }
 
