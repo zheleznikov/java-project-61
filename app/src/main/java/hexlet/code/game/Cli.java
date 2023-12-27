e2f35816a838faa5bead252751ce2b30569844b8
Сергей <polwen@yandex.ru>
2023-12-27 18:30:44 +0300

    fix greeting

--- app/src/main/java/hexlet/code/game/Cli.java
+++ app/src/main/java/hexlet/code/game/Cli.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Cli {
 
     public static String greetUser(Scanner scanner) {
-        System.out.println("Welcome to the Brain IsEvenGame!");
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.nextLine();
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + "!");
         return name;
     }
 
